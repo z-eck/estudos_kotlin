24d6d9087085d88170723aa6ed7093e18734daec
Zeck <erick_escarlate@outlook.com>
2022-02-23 09:38:55 -0300

    Criando repositório e adicionando a primeira atividade de variavel e tipos de dados

---
+++ src/Atividade1_Variaveis_TiposDeDados.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var nomeCompleto: String = "Erick O. Souza"
+    println("Meu nome é: $nomeCompleto")
+    var texto: String = ""
+    println("O que tem nesse texto? $texto")
+    val numeroCalcado: Short = 44
+    println("Eu calço $numeroCalcado")
+    var numeroPIB: Long = 1869000000000
+    var popBr: Int = 211000000
+
+    println("O Valor do PIB per Capita é: ${numeroPIB / popBr}")
+}
