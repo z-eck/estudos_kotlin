658454688fbdb654456e136d4293686e0cd6525f
Zeck <erick_escarlate@outlook.com>
2022-04-01 11:08:50 -0300

    adicionando algumas atividades que estavam salvas no pc do meu pai

---
+++ src/Atividade3_1_Controle_de_Fluxo_If_Else.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+}
+
+//1) Escreva um programa para ajudar a empresa XPTO calcular o bônus que os funcionários receberão no
+//final do ano. Os bônus são classificados por cargo.
+//a. Gerentes recebem R$ 2.000,00
+//b. Coordenadores recebem R$ 1.500,00
+//c. Engenheiros de software recebem R$ 1.000,00
+//d. Estagiários recebem R$ 500,00
+
+//2) Modifique o primeiro exercício para considerar o tempo de experiência no cálculo de bônus.
+//a. Gerentes com menos de 2 anos de experiência recebem R$ 2.000,00, caso contrário recebem R$ 3.000,00
+//b. Coordenadores com menos de 1 ano de experiência recebem R$ 1.500,00, caso contrário recebem R$ 1.800,00
+//c. Engenheiros de software recebem R$ 1.000,00
+//d. Estagiários recebem R$ 500,00
